5334bfcb980cc13e0f3db38e609b4f7113c839f2
Bersabel Tadesse <brsabel.t@gmail.com>
2026-07-16 22:16:28 -0700

    Tighten sidebar display menu

--- apps/app/src/components/sidebar/ProjectList.tsx
+++ apps/app/src/components/sidebar/ProjectList.tsx
@@ -652,7 +652,6 @@ export function SidebarDisplayOptionsMenu({
       <DropdownMenuContent
         align="end"
         mobileTitle="Display options"
-        className="min-w-[13rem]"
       >
         <DropdownMenuLabel className={CHROME_SECTION_LABEL_CLASS}>
           Organize
