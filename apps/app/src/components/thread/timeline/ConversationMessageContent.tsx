7fe2591167252fa21dbd5ef5570a9950446848d8
Michael Yong <wrong92@gmail.com>
2026-08-10 13:33:48 -0700

    Add experimental sent-message editing

--- apps/app/src/components/thread/timeline/ConversationMessageContent.tsx
+++ apps/app/src/components/thread/timeline/ConversationMessageContent.tsx
@@ -86,6 +86,7 @@ export interface ConversationMessageContentUserProps extends ConversationMessage
   initiator: TimelineUserConversationRow["initiator"];
   mentions: readonly PromptTextMention[];
   onAddToChat?: ThreadTimelineAddToChatHandler;
+  onEdit?: () => void;
   resolveMentionLink?: PromptMentionLinkResolver;
   resolveSegmentLinkHref?: TimelineTitleLinkResolver;
   onOpenLink?: ThreadTimelineLinkHandler;
@@ -187,6 +188,7 @@ interface UserConversationMessageProps {
   mentions: readonly PromptTextMention[];
   mobileActionDisplay: "inline" | "overflow";
   onAddToChat?: ThreadTimelineAddToChatHandler;
+  onEdit?: () => void;
   onOpenLink?: ThreadTimelineLinkHandler;
   onOpenLocalFileLink?: ThreadTimelineLocalFileLinkHandler;
   projectId?: string;
@@ -376,6 +378,7 @@ function UserConversationMessage({
   mentions,
   mobileActionDisplay,
   onAddToChat,
+  onEdit,
   onOpenLink,
   onOpenLocalFileLink,
   pluginActions = [],
@@ -491,6 +494,7 @@ function UserConversationMessage({
         </div>
         {messageText ||
         addToChatAttachments.length > 0 ||
+        onEdit !== undefined ||
         pluginActions.length > 0 ? (
           <div className="mt-1 flex justify-end">
             <MessageActionBar
@@ -499,6 +503,7 @@ function UserConversationMessage({
               mobileActionDisplay={mobileActionDisplay}
               addToChatAttachments={addToChatAttachments}
               onAddToChat={onAddToChat}
+              onEdit={onEdit}
               pluginActions={pluginActions}
             />
           </div>
@@ -711,6 +716,7 @@ export function ConversationMessageContent(
         mentions={props.mentions}
         mobileActionDisplay={props.mobileActionDisplay ?? "overflow"}
         onAddToChat={props.onAddToChat}
+        onEdit={props.onEdit}
         onOpenLink={props.onOpenLink}
         onOpenLocalFileLink={onOpenLocalFileLink}
         projectId={projectId}
