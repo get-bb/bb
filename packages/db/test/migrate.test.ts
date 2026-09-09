a8bf1f6d5efda387018d1cf5447b7b04c93af1be
Michael Yong <wrong92@gmail.com>
2026-09-08 20:36:02 -0700

    Align machine APIs with updated main contracts and migrations

--- packages/db/test/migrate.test.ts
+++ packages/db/test/migrate.test.ts
@@ -1645,7 +1645,6 @@ describe("migrate", () => {
       const host = upsertHost(db, noopNotifier, {
         id: "host-retained-output-migration",
         name: "Migration Host",
-        type: "persistent",
       });
       const { project } = createProject(db, noopNotifier, {
         name: "Migration Project",
@@ -1884,6 +1883,7 @@ describe("migrate", () => {
         defaultProviderId: null,
         machineServerUrl: null,
         defaultMachineAccess: null,
+        machineGitCredentialsEnabled: true,
         streamerMode: false,
         managedBranchPrefix: "bb/",
       });
